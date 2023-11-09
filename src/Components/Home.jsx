8f7ef9be1634ea82c0ab046838db77543f981960
ifeoluwa02 <88455435+ifeoluwa02@users.noreply.github.com>
2023-11-09 13:33:39 +0000

    Pending changes exported from your codespace

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,7 +13,7 @@ import PropTypes from "prop-types";
  * Home background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that '
  * represents what you see in that image.
  *
  *
@@ -23,12 +23,12 @@ import PropTypes from "prop-types";
 // import image from "../images/woman-with-tablet.jpg";
 import image from "../images/luca-bravo-XJXWbfSo2f0-unsplash.jpg";
 
-
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText =
+  "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
 
 const Home = ({ name, title }) => {
   return (
-    <section id="home" className="min-height  " style={{color:"white"}}>
+    <section id="home" className="min-height  " style={{ color: "white" }}>
       <img className="background" src={image} alt="" />
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
         <h1>{name}</h1>
